869f825d8c64d455ffc083290caf615f12ea6e08
hungtrinh245 <trinhquochung222005@gmail.com>
2025-06-09 23:29:19 +0700

    hằng số-khung thời gian

---
+++ src/utils/constants.js
@@ -0,0 +1,6 @@
+
+export const INTERVALS = [
+    '1m', '5m', '15m', '30m', '1h', '2h', '4h', '6h', '8h', '12h', '1d', '3d', '1w', '1M'
+];
+
+export const SYMBOL = 'BTCUSDT'; // Cặp tiền tệ mặc định
